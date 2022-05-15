0d9618581516e18bcf11d1b9fd7d1a0e75231507
LauraRendon-Garcia <82424794+LauraRendon-Garcia@users.noreply.github.com>
2022-05-14 22:20:13 -0400

    added in gsap timeline

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,11 @@
 // import { gsap } from "gsap";
+
+import { gsap } from "gsap";
+import { Flip } from "gsap/Flip";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+
+gsap.registerPlugin(Flip, MotionPathPlugin, DrawSVGPlugin, Physics2DPlugin);
+
+const mainTL = gsap.timeline();
+
+GSDevTools.create();
