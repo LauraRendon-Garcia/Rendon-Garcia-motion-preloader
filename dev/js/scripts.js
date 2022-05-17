f74dfeae04b2b3ef69dd0c04d66db86e3cd08043
LauraRendon-Garcia <82424794+LauraRendon-Garcia@users.noreply.github.com>
2022-05-16 21:15:11 -0400

    tail and body

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,10 +2,48 @@
 
 import { gsap } from "gsap";
 import { Flip } from "gsap/Flip";
+import { CustomWiggle, CustomEase } from "gsap/all";
+// import { CustomWiggle } from "gsap/CustomWiggle";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { DrawSVGPlugin} from "gsap/DrawSVGPlugin";
+import { Physics2DPlugin } from "gsap/Physics2DPlugin";
+// import {GSDevTools} from "gsap/GSDevTools"
 
-gsap.registerPlugin(Flip, MotionPathPlugin, DrawSVGPlugin, Physics2DPlugin);
 
-const mainTL = gsap.timeline();
 
-GSDevTools.create();
+
+// CustomWiggle.create("tailWiggle", {wiggles: 6});
+gsap.registerPlugin(Flip, MotionPathPlugin, DrawSVGPlugin, Physics2DPlugin, CustomWiggle, CustomEase);
+
+// const mainTL = gsap.timeline();
+// const myNum = 5;
+
+// console.log("myNum1 ="+ myNum1a);
+
+// function body(){
+//     const myNum2 = 8;
+//     console.log("myNum1 ="+ myNum1a);
+//     console.log("myNum2 ="+ myNum2);
+// }
+
+function body(){
+
+    CustomWiggle.create("tailWiggle", {wiggles: 8});
+    let tl = gsap.timeline();
+    tl.to("#logo", {duration:1,y:-100}, 'moveToTop');
+    tl.to("#tail", {duration:1,y:20, ease: "tailWiggle"}, 'moveToTop')
+    tl.to("#logo", {duration:1,y:120});
+    tl.to("#logo", {duration:1,y:0});
+
+    return tl; 
+
+}
+//console.log("myNum2 ="+ myNum2);
+
+body();
+
+
+// GSDevTools.create();
+
+ // mainTL
+ // ;
